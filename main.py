985224f7c47a0855b6b2172d45e7f085cabbd3e8
lzkostina <kostina@umich.edu>
2024-11-13 20:37:29 -0500

    november 13

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import numpy as np
 import datetime
 
-import utils
+from predictor import utils
 from predictor import TestPredictor
 
 import logging
