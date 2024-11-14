17b00d2aed284c249d8f6f42b209f55fc2f949c7
lzkostina <kostina@umich.edu>
2024-11-13 19:44:10 -0500

    november 13

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import utils
 from predictor import TestPredictor
 
 import logging
+
+
+if __name__ == "__main__":
+
+    model = TestPredictor()
+    data = []
+    predictions = model.predict(data)
+    
+    prediction_date = f"{datetime.date.today():%Y-%m-%d}"
+    predictions_rounded = np.around(predictions, 1)
+    
+    fmt_str_contents = [prediction_date] + list([str(prediction) for prediction in predictions_rounded])
+    fmt_str = ", ".join(fmt_str_contents)
+    print(fmt_str)
